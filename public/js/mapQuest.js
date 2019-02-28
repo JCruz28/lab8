ca1a118a88f7b6e2c7d3eacc5d81282fe2525c49
Justin Cruz <jtc027@ucsd.edu>
2019-02-28 10:56:27 -0800

    Lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'GlAFJFjkMAjp5U63MVFmC3ZyWnBR3cGG';
+
+	var map = L.mapquest.map('map', {
+		center: [32.878713, -117.236202],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	L.marker([32.878713, -117.236202]).addTo(map);
 }
